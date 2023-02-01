fcb8a3fedda3811393d5826bf58d9b3298373ad2
Vova Gusev <gusevvovik@gmail.com>
2023-02-01 20:02:51 +0200

    Added adress generator

---
+++ text_generator/adress_generator.py
@@ -0,0 +1,30 @@
+import argparse
+import pandas as pd
+from faker import Faker
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Text generator')
+    parser.add_argument("--templates_path", help="Path to the templates")
+    parser.add_argument("--result_path", help="Path to the result_folder")
+    parser.add_argument("--random_seed", help="Random seed")
+    parser.add_argument("--samples_amount", help="Amount of samples to generate")
+    args = parser.parse_args()
+    templates_path = args.templates_path
+    result_path = args.result_path
+    random_seed = args.random_seed
+    samples_amount = args.samples_amount
+
+    fake = Faker('uk_UA')
+    Faker.seed()
+    dataframe = pd.read_excel(templates_path)
+
+    with open('input.txt', 'w') as x:
+        with open('target.txt', 'w') as y:
+            for i in range(len(dataframe['input'])):
+                for j in range(int(samples_amount)):
+                    adress = fake.address()
+                    x.write(dataframe['input'][i].replace('?adress', adress))
+                    x.write('\n')
+                    y.write(dataframe['target'][i].replace('?adress', adress))
+                    y.write('\n')
